63c9575c042a460a8abcdc102b112fed819dab53
Elena <Shherbakove@yandex.ru>
2023-04-22 23:10:42 +0300

    Домашка к 5 семинару корректировка задачи 28_2

--- Task28.py
+++ Task28.py
@@ -7,11 +7,23 @@
 # 2 2
 #     4
 
-def sum(a, b):
-    if b == 0:
-        return a
-    return sum(a+1, b-1)
+# def sum(a, b):
+#     if b == 0:
+#         return a
+#     return sum(a+1, b-1)
 
-a = int(input("Введите число A: "))
-b = int(input("Введите число B: "))
+# a = int(input("Введите число A: "))
+# b = int(input("Введите число B: "))
+# print(sum(a, b))
+
+
+def sum(a, b): 
+    if b == 0: 
+        return a 
+    a +=1
+    b -= 1
+    return sum(a, b) 
+
+a = int(input("Введите число A: ")) 
+b = int(input("Введите число B: ")) 
 print(sum(a, b))
